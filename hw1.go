080e4f4904e72d5277aefa8ab08faf236f89b434
mikhaelgracius <mikhaelbratanata@Mikes-MBP.local>
2015-03-15 15:45:24 -0700

    hw1
    pointers, types, and structs

---
+++ hw1.go
@@ -0,0 +1,38 @@
+// hw1 project main.go
+package main
+
+import "fmt"
+
+type hello string //type
+
+type likeClass struct { //struct
+	language string
+	compiler string
+}
+
+func main() {
+	var math hello = "Mathematics"
+	fmt.Println(math)
+
+	var input string
+	input = "This is my first Go program."
+
+	var inputAddress *string = &input //pointers
+	var sameInput *string = &input
+
+	fmt.Println(input)
+	fmt.Println(inputAddress)
+	fmt.Println(*sameInput)
+
+	*sameInput = "This language is pretty cool."
+
+	fmt.Println(*sameInput)
+
+	//-------------------//
+
+	var intro = likeClass{compiler: "LiteIDE", language: "GO"}
+
+	fmt.Println(intro.language)
+	fmt.Println(intro.compiler) // you can't combine the intro.language and intro.compiler in one single fmt.Println
+
+}
